0011e532b47908b20155999896e1d3940fa8eb99
charin Tantrakul <charin.ta@ku.th>
2017-01-27 16:30:33 +0700

    edit javadoc

--- src/guessinggame/GuessingGame.java
+++ src/guessinggame/GuessingGame.java
@@ -1,8 +1,9 @@
 package guessinggame;
 import java.util.Random;
 /**
- * Game of guessing a secret number
+ * Game of guessing a secret number.
  * @author Charin Tantrakul
+ * @version 26.01.2017
  */
 public class GuessingGame {
 	/* properties of a guessing game*/
@@ -11,7 +12,7 @@ public class GuessingGame {
 	private String hint;
 	int count=0;
 	/**
-	 * set defalut of variable upperbound secret and hint
+	 * Set defalut of variable upperbound, secret and hint.
 	 */
 	public GuessingGame(int upperbound){
 		this.upperbound = upperbound;
@@ -21,7 +22,7 @@ public class GuessingGame {
 	/**
 	 * Create a random number between 1 and limit.
 	 * @param limit is the limit for random number
-	 * @return a random number between 1 and limit (inclusive)
+	 * @return a secret number.
 	 */
 	private int getRandomNumber(int limit){
 		long seed = System.currentTimeMillis();
@@ -30,8 +31,8 @@ public class GuessingGame {
 	}
 	/**
 	 * Guess input their number to check
-	 * @param number is the number that input by user
-	 * @return true if number match with secret return false if number not match with secret
+	 * @param number is the number that inputed by user
+	 * @return true if number match with secret and false if number not match with secret
 	 */
 	public boolean guess(int number){
 		if(number==this.secret){
@@ -51,22 +52,22 @@ public class GuessingGame {
 		}
 	}
 	/**
-	 * Return a hint based on the most recent guess
-	 * @return hint based on most recent guess
+	 * Return a hint based on the most recent guess.
+	 * @return hint as a string.
 	 */
 	public String getHint(){
 		return this.hint;
 	}
 	/**
-	 * Set hint depend on  answer of the player and upperboundnumber
-	 * @param hint depend on  answer of the player and upperboundnumber
+	 * Set hint depend on  answer of the player and upperboundnumber.
+	 * @param hint is received as a String.
 	 */
 	protected void setHint(String hint){
 		this.hint = hint;
 	}
 	/**
 	 * getCount() return total number of answers that players input.
-	 * @return count is total number of answers that players input.
+	 * @return count
 	 */
 	public int getCount(){
 		return this.count;
